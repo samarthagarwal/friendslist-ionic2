72774cb35a575e12b8ea631b1cf40141011118b5
Samarth Agarwal <samarthagarwal@live.com>
2016-10-24 23:54:35 +0530

    Lec13-Organizing Folder and Files

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { LoginPage } from '../pages/login/login';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    LoginPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    LoginPage
+  ],
+  providers: []
+})
+export class AppModule {}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
