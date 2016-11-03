642e84329053f2a2ec565ca59a9989d7adca0721
Samarth Agarwal <samarthagarwal@live.com>
2016-11-04 00:39:54 +0530

    finalized saving data to parse

--- src/pages/list-page/list-page.ts
+++ src/pages/list-page/list-page.ts
@@ -1,22 +1,73 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the ListPage page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-list-page',
   templateUrl: 'list-page.html'
 })
 export class ListPage {
 
-  constructor(public navCtrl: NavController) {}
+  headers: Headers;
+  url: string;
 
-  ionViewDidLoad() {
-    console.log('Hello ListPage Page');
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public http: Http) {
+    this.headers = new Headers();
+    this.headers.append("X-Parse-Application-Id", "AppId1");
+  }
+
+  showAddDialog(){
+    this.alertCtrl.create({
+      title: "Add Friend",
+      message: "Enter the information of new friend",
+      inputs: [{
+        name: 'name',
+        placeholder: 'Enter the name'
+      },{
+        name: 'email',
+        placeholder: "Enter the email"
+      }, {
+        name: "number",
+        placeholder: "Enter the number"
+      }],
+      buttons: [
+        {
+          text: "Cancel"
+        },{
+          text: "Save",
+          handler: data => {
+            //post the information to parse server
+
+            this.url = "https://parsewithionic-samarthagarwal.c9users.io/app1/classes/friendslist";
+
+            this.http.post(this.url, { name: data.name, email: data.email, number: data.number, image: "http://lorempixel.com/32/32"}, {headers: this.headers}).map(res => res.json()).subscribe(res => {
+                console.log(res);
+                this.alertCtrl.create({
+                  title: "Success",
+                  message: "Friend Infomration Saved Successfully.",
+                  buttons: [{
+                    text: "OK",
+                    handler: ()=>{
+
+                    }
+                  }]
+                }).present();
+            }, err => {
+                console.log(err);
+                this.alertCtrl.create({
+                  title: "Error",
+                  message: err.text(),
+                  buttons: [{
+                    text: "OK"
+                  }]
+                }).present();
+            })
+
+          }
+        }
+      ]
+    }).present();
   }
 
 }
