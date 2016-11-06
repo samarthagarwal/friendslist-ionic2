454b13613d524dd2bda3e56fd7060431f0b2194f
Samarth Agarwal <samarthagarwal@live.com>
2016-11-06 15:06:48 +0530

    added editing/deleting/logout feature

--- src/pages/list-page/list-page.ts
+++ src/pages/list-page/list-page.ts
@@ -3,6 +3,7 @@ import { NavController, AlertController } from 'ionic-angular';
 import { Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Storage } from '@ionic/storage';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-list-page',
@@ -102,8 +103,105 @@ export class ListPage {
   }
 
 
-  
+  editFriend(friend){
+
+    this.alertCtrl.create({
+      title: "Edit Friend",
+      message: "Edit your friends information here",
+      inputs: [{
+        name: 'name',
+        placeholder: 'Enter the name',
+        value: friend.name
+      },{
+        name: 'email',
+        placeholder: "Enter the email",
+        value: friend.email
+      }, {
+        name: "number",
+        placeholder: "Enter the number",
+        value:friend.number
+      }],
+      buttons: [{
+        text: "Cancel"
+      },{
+        text: "Save",
+        handler: data => {
+           //perform update on parse server here
+          
+           this.url = "https://parsewithionic-samarthagarwal.c9users.io/app1/classes/friendslist/" + friend.objectId;
+           
+           this.http.put(this.url, {name: data.name, email: data.email, number: data.number}, {headers: this.headers}).map(res => res.json()).subscribe(
+             res => {
+               console.log(res);
+              this.alertCtrl
+                .create({ title : "Success", message: "Friend Updated Successfully.", buttons: [{
+                  text: 'OK',
+                  handler: ()=>{
+                    this.getFriends(null);
+                  }
+                }]})
+                .present();
+             },
+             err => {
+               console.log(err);
+                this.alertCtrl
+                  .create({ title : "Error", message: err.text(), buttons: [{
+                    text: 'OK',
+                  }]})
+                  .present();
+             }
+           )
+        }
+      }]
+    }).present();
 
 
+  }
+
+  deleteFriend(friend){
+    this.alertCtrl.create({
+      title: "Delete Friend",
+      message: "Are you sure?",
+      buttons: [{
+        text: "No"
+      },{
+        text: "Yes",
+        handler: () => {
+           //perform delete on parse server here
+          
+           this.url = "https://parsewithionic-samarthagarwal.c9users.io/app1/classes/friendslist/" + friend.objectId;
+           
+           this.http.delete(this.url, {headers: this.headers}).map(res => res.json()).subscribe(
+             res => {
+               console.log(res);
+              this.alertCtrl
+                .create({ title : "Success", message: "Friend Deleted Successfully.", buttons: [{
+                  text: 'OK',
+                  handler: ()=>{
+                    this.getFriends(null);
+                  }
+                }]})
+                .present();
+             },
+             err => {
+               console.log(err);
+                this.alertCtrl
+                  .create({ title : "Error", message: err.text(), buttons: [{
+                    text: 'OK',
+                  }]})
+                  .present();
+             }
+           )
+        }
+      }]
+    }).present();
+  }
+
+
+  logout(){
+    this.localStorage.remove('user').then(() => {
+      this.navCtrl.setRoot(LoginPage);
+    });
+  }
 
 }
